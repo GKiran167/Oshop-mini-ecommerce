4b9bdb323fa667f077b01c3e921a93740609959c
GKiran167 <official.kirankumar167@gmail.com>
2024-04-20 13:59:53 +0530

    !feat : Link added to product images

--- frontend/src/components/ProductCard.js
+++ frontend/src/components/ProductCard.js
@@ -1,26 +1,26 @@
 import { Link } from "react-router-dom";
 
 
-export default function ProductCard({product}) {
+export default function ProductCard({ product }) {
     // const {product} = props;
     return <>
         <div className="col-sm-12 col-md-6 col-lg-3 my-3">
             <div className="card p-3 rounded">
-                <img
+                <Link to={"/product/" + product._id}> <img
                     className="card-img-top mx-auto"
                     src={product.images[0].image}
-                />
+                /></Link>
                 <div className="card-body d-flex flex-column">
                     <h5 className="card-title">
-                        <Link to={"/product/"+product._id}>{product.name}</Link>
+                        <Link to={"/product/" + product._id}>{product.name}</Link>
                     </h5>
                     <div className="ratings mt-auto">
                         <div className="rating-outer">
-                            <div className="rating-inner" style={{width :`${product.ratings/5 * 100}%`}}></div>
+                            <div className="rating-inner" style={{ width: `${product.ratings / 5 * 100}%` }}></div>
                         </div>
                     </div>
                     <p className="card-text">₹{product.price}</p>
-                    <Link to={"/product/"+product._id} id="view_btn" className="btn btn-block">View Details</Link>
+                    <Link to={"/product/" + product._id} id="view_btn" className="btn btn-block">View Details</Link>
                 </div>
             </div>
         </div>
